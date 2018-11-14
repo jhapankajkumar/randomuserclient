fff982a5a24a90f9a26a7cac83f9b1f46f3ac5ea
Gyan <sgmshd04@Gyans-Mac-mini.local>
2018-11-14 13:07:34 +0800

    initial project initialized

---
+++ RandomUserApp/RandomUserApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RandomUserApp
+//
+//  Created by Gyan on 14/11/18.
+//  Copyright © 2018 Pankaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RandomUserApp/RandomUserApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RandomUserApp
+//
+//  Created by Gyan on 14/11/18.
+//  Copyright © 2018 Pankaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
